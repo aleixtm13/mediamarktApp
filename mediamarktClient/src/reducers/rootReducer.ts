6a6bff2bd91bd6d3249c3184c666bde8faa2938f
aleixtm13 <aleix13t13m@gmail.com>
2023-11-27 15:28:39 +0100

    add: redux setup

---
+++ mediamarktClient/src/reducers/rootReducer.ts
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'
+
+// import productReducer from '../reducers/productReducer' //TODO: uncomment when productReducer is created
+
+const rootReducer = combineReducers({})
+
+export default rootReducer
+
