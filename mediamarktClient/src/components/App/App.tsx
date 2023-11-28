5fb18069a724b3fcf0d627fe6e941dfa1dd928ca
aleixtm13 <aleix13t13m@gmail.com>
2023-11-28 16:17:03 +0100

    add: tailwind css and base boilerplate

---
+++ mediamarktClient/src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import Products from '../Products/Products'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <header className='bg-red-600 text-white'>
+      <h1>Mediamarkt App</h1>
+     </header>
+     <Products />
+    </>
+  )
+}
+
+export default App
--- mediamarktClient/src/components/Root/root.tsx
+++ mediamarktClient/src/components/Root/root.tsx
@@ -1,4 +1,4 @@
-import App from './App.tsx'
+import App from '../App/App.tsx'
 
 export default function Root() {
     return (
