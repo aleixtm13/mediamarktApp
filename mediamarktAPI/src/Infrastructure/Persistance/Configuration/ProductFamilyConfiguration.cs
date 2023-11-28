9fd7e213e3aa2ab8882218c44875fb850a43a6a1
aleixtm13 <aleix13t13m@gmail.com>
2023-11-28 12:00:10 +0100

    add: initial migrations

---
+++ mediamarktAPI/src/Infrastructure/Persistance/Configuration/ProductFamilyConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistance.Configuration
+{
+    public class ProductFamilyConfiguration : IEntityTypeConfiguration<ProductFamily>
+    {
+        public void Configure(EntityTypeBuilder<ProductFamily> builder)
+        {
+            builder.HasKey(productFamily => productFamily.Id);
+            builder.Property(productFamily => productFamily.Id)
+                .HasConversion(productFamily => productFamily.Value,
+                value => new ProductFamilyId(value));
+            builder.HasMany(productFamily => productFamily.Products);
+        }
+    }
+}
---
+++ mediamarktAPI/src/Infrastructure/Persistance/Migrations/20231128104802_InitialMigration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Planets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Orbit_OrbitalRadius = table.Column<double>(type: "float", nullable: false, defaultValue: 0.0),
+                    Orbit_OrbitalPeriod = table.Column<double>(type: "float", nullable: false, defaultValue: 0.0),
+                    Orbit_RotationPeriod = table.Column<double>(type: "float", nullable: false, defaultValue: 0.0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Planets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductFamily",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductFamily", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    ProductFamilyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_ProductFamily_ProductFamilyId",
+                        column: x => x.ProductFamilyId,
+                        principalTable: "ProductFamily",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductFamilyId",
+                table: "Products",
+                column: "ProductFamilyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Planets");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "ProductFamily");
+        }
+    }
+}
