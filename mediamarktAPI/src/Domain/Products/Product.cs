5e7c1e83ed39500509ffe7934b3effec6423fbc3
aleixtm13 <aleix13t13m@gmail.com>
2023-11-28 09:39:17 +0100

    add: price attribute to Product Entity

--- mediamarktAPI/src/Domain/Products/Product.cs
+++ mediamarktAPI/src/Domain/Products/Product.cs
@@ -5,11 +5,12 @@ namespace Domain.Products;
 public sealed class Product : AggregateRoot 
 {
 
-    public Product(ProductId productId, string name, string description, ProductFamilyId productFamilyId, ProductFamily productFamily)
+    public Product(ProductId productId, string name, string description, double price, ProductFamilyId productFamilyId, ProductFamily productFamily)
     {
         ProductId = productId;
         Name = name;
         Description = description;
+        Price = price;
         ProductFamilyId = productFamilyId;
         ProductFamily = productFamily;
     }
@@ -17,7 +18,7 @@ public sealed class Product : AggregateRoot
     public ProductId ProductId {get; private set;}
     public string Name {get; private set;} = string.Empty;
     public string Description {get; private set;} = string.Empty;
-
+    public double Price {get; private set;}
     public ProductFamilyId ProductFamilyId {get; private set;}
     public ProductFamily ProductFamily { get; private set;}
 }
