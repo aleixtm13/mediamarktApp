89e2ce2b2bd3795499936ecf60753bcd9a6e31d5
aleixtm13 <aleix13t13m@gmail.com>
2023-11-28 16:54:31 +0100

    add: products base component

--- mediamarktClient/src/components/App/App.tsx
+++ mediamarktClient/src/components/App/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
      <header className='bg-red-600 text-white'>
-      <h1>Mediamarkt App</h1>
+      <h1 className='text-center'>Mediamarkt App</h1>
      </header>
      <Products />
     </>
--- mediamarktClient/src/components/Products/Products.tsx
+++ mediamarktClient/src/components/Products/Products.tsx
@@ -1,7 +1,34 @@
+import { Product } from '../../Model/Product'
+import { DataTable } from 'primereact/datatable'
+import { Column } from 'primereact/column'
+
+const products: Product[] = [
+    {
+        id: "1",
+        name: "iPhone 12",
+        description: "Apple iPhone 12 64GB Zwart",
+        price: 909,
+        productFamily: "Smartphone"
+    },
+    {
+        id: "2",
+        name: "Delonghi Dedica",
+        description: "Delonghi Dedica EC685.BK",
+        price: 199.9,
+        productFamily: "coffee machine"
+    },
+]
+
 function Products() {
     return (
         <div>
-            <h1>Products</h1>
+            <div className='container'>
+                <DataTable value={products}  tableStyle={{ minWidth: '50rem' }}>
+                    <Column field="name" header="Name"></Column>
+                    <Column field="productFamily" header="Product family"></Column>
+                </DataTable>
+            </div>
+           
         </div>
     )
 }
