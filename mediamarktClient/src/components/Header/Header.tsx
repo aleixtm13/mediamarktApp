f1d50b87452ec6ee92ef8c83b5766dd72de4b38f
aleixtm13 <aleix13t13m@gmail.com>
2023-11-29 16:31:16 +0100

    add: red theme

--- mediamarktClient/src/components/Header/Header.tsx
+++ mediamarktClient/src/components/Header/Header.tsx
@@ -2,8 +2,7 @@ const Header: React.FC = () => {
     return (
     <div className="bg-red-500 text-white">
         <div className="container mx-auto py-4">
-        <h1 className="text-2xl font-semibold text-center">MediaMarkt</h1>
-        {/* Add additional header elements as needed */}
+            <h1 className="text-2xl font-semibold text-center">MediaMarkt</h1>
         </div>
     </div>
     )
