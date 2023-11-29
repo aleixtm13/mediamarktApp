5a28642842c4a964f6ed195b210db76237558635
aleixtm13 <aleix13t13m@gmail.com>
2023-11-29 17:22:57 +0100

    update: header color to primary color

--- mediamarktClient/src/components/Header/Header.tsx
+++ mediamarktClient/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 const Header: React.FC = () => {
     return (
-    <div className="bg-red-500 text-white">
+    <div className="text-white" style={{backgroundColor: 'var(--primary-color)'}}>
         <div className="container mx-auto py-4">
             <h1 className="text-2xl font-semibold text-center">MediaMarkt</h1>
         </div>
