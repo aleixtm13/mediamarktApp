a7b9dbbc64c657f6ec643fc56b3f11985b94c097
aleixtm13 <aleix13t13m@gmail.com>
2023-11-29 08:55:05 +0100

    add: cors policiy to the API

--- mediamarktAPI/src/Web.API/Program.cs
+++ mediamarktAPI/src/Web.API/Program.cs
@@ -10,6 +10,11 @@ builder.Services.AddPresentation()
     .AddInfrastructure(builder.Configuration)
     .AddApplication();
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +29,9 @@ app.UseExceptionHandler("/error");
 
 app.UseHttpsRedirection();
 
+app.UseCors("corsapp");
 app.UseAuthorization();
+
 
 app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
