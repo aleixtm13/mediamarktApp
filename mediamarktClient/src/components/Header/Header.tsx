c352c862f86b46db473de8d2291d49cf102c50fd
aleixtm13 <aleix13t13m@gmail.com>
2023-11-30 11:12:01 +0100

    add: UTs for header component

--- mediamarktClient/src/components/Header/Header.tsx
+++ mediamarktClient/src/components/Header/Header.tsx
@@ -2,11 +2,16 @@ import { Button } from "primereact/button"
 import { useNavigate } from "react-router";
 const Header: React.FC = () => {
     const navigate = useNavigate()
+    const onAppLogoClick = () => {
+        navigate('/')
+    }
     return (
     <div className="text-white" style={{backgroundColor: 'var(--primary-color)'}}>
         <div className="container mx-auto py-4 flex justify-center">
             <Button
-            onClick={() => navigate('/')}
+            id="logo-button"
+            data-testid="logo-button"
+            onClick={onAppLogoClick}
             >
                 <h1 className="text-2xl font-semibold">MediaMarkt</h1>
             </Button>
